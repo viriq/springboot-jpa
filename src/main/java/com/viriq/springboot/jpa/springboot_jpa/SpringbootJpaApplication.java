f99aeb6cc7f22d4ddd58323e28118f8e38d8f09c
viriq <vi.riquelmea@duocuc.cl>
2025-05-15 15:25:25 -0400

    Implementado filtrado Persons por ProgrammingLanguage

--- src/main/java/com/viriq/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/viriq/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -22,7 +22,7 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		List<Person> persons = (List<Person>)repository.findAll();
+		List<Person> persons = (List<Person>)repository.findByLanguages("Java");
 
 		persons.stream().forEach(person -> {
 			System.out.println(person);
--- src/main/java/com/viriq/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/viriq/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,8 +1,13 @@
 package com.viriq.springboot.jpa.springboot_jpa.repositories;
 
 import com.viriq.springboot.jpa.springboot_jpa.entities.Person;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    List<Person> findByLanguages(String languages);
 
 }
